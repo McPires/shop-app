e702cc88cf5224282df5ef84f8d6cf9dd055e56d
Rui <ruimcpires@gmail.com>
2022-03-07 12:15:07 +0000

    Create a reusable component

--- components/shop/OrderItem.js
+++ components/shop/OrderItem.js
@@ -2,12 +2,13 @@ import { Button, StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import Colors from "../../constants/Colors";
 import CartItem from "./CartItem";
+import Card from "../UI/Card";
 
 const OrderItem = (props) => {
   const [showDetails, setShowDetails] = useState(false);
 
   return (
-    <View style={styles.orderItem}>
+    <Card style={styles.orderItem}>
       <View style={styles.summary}>
         <Text style={styles.amount}>${props.amount.toFixed(2)}</Text>
         <Text style={styles.date}>{props.date.toString()}</Text>
@@ -31,7 +32,7 @@ const OrderItem = (props) => {
           ))}
         </View>
       )}
-    </View>
+    </Card>
   );
 };
 
@@ -39,13 +40,6 @@ export default OrderItem;
 
 const styles = StyleSheet.create({
   orderItem: {
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 8,
-    elevation: 5,
-    borderRadius: 10,
-    backgroundColor: "white",
     margin: 20,
     padding: 10,
     alignItems: "center",
